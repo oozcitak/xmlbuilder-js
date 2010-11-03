61663817400e171cfabb8458e4d715d151832684
oozcitak <oozcitak@gmail.com>
2010-11-03 19:12:58 +0000

    added a simple test

---
+++ test/test.js
@@ -0,0 +1,27 @@
+assert = require('assert');
+
+var builder = require('./lib/index.js').builder();
+var xml = '<root>' +
+            '<xmlbuilder for="node-js" awesome="CoffeeScript">' +
+              '<repo type="git">git://github.com/oozcitak/xmlbuilder-js.git</repo>' +
+            '</xmlbuilder>' +
+	    '<test>complete</test>' +
+          '</root>';
+
+builder.begin()
+  .ele('root')
+    .ele('xmlbuilder')
+      .att('for', 'node-js')
+      .att('awesome', 'CoffeeScript')
+      .ele('repo')
+        .att('type', 'git')
+        .txt('git://github.com/oozcitak/xmlbuilder-js.git')	
+      .up()
+    .up()
+  .up()
+  .ele('test')
+    .txt('complete');
+
+var test = builder.toString();
+assert.ok(xml === test);
+
