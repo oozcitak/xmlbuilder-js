f0a06c027d9ece5a555c0064a28baef9d26f6f73
oozcitak <oozcitak@gmail.com>
2010-12-27 00:53:09 +0200

    Removed tab chars.

--- test/test.js
+++ test/test.js
@@ -1,26 +1,25 @@
-assert = require('assert');
-
-var builder = require('../lib/index.js').builder();
 var xml = '<root>' +
             '<xmlbuilder for="node-js" awesome="CoffeeScript">' +
               '<repo type="git">git://github.com/oozcitak/xmlbuilder-js.git</repo>' +
             '</xmlbuilder>' +
-	    '<test>complete</test>' +
+            '<test>complete</test>' +
           '</root>';
 
+var builder = require('../lib/index.js').builder();
 builder.begin('root')
   .ele('xmlbuilder')
     .att('for', 'node-js')
     .att('awesome', 'CoffeeScript')
     .ele('repo')
       .att('type', 'git')
-      .txt('git://github.com/oozcitak/xmlbuilder-js.git')	
+      .txt('git://github.com/oozcitak/xmlbuilder-js.git')
     .up()
   .up()
 .up()
 .ele('test')
   .txt('complete');
-
 var test = builder.toString();
+
+var assert = require('assert');
 assert.ok(xml === test);
 
